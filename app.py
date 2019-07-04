c28260f76ad9c61c2983557378f85e720780c90f
Ubuntu <ubuntu@ip-172-31-91-138.ec2.internal>
2019-07-04 15:20:20 +0000

    added Add Recipe form and connected to mongodb so completed form is added to database on submission

--- app.py
+++ app.py
@@ -13,6 +13,20 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-aogax.mong
 mongo = PyMongo(app) 
 
 @app.route('/')
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html', cuisine=mongo.db.cuisine.find())
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes=mongo.db.recipe
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+
+
+
+
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipe.find())
