57280e19380f0adb4fdc66ecb9bacd04d5cafc5b
Ubuntu <ubuntu@ip-172-31-91-138.ec2.internal>
2019-07-04 12:29:01 +0000

    linked app to database

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask , render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+db_password = os.environ.get('DB_PASSWORD')
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'recipe_project'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-aogax.mongodb.net/recipe_project?retryWrites=true&w=majority'
+
+mongo = PyMongo(app) 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipe.find())
+    
 
 
 if __name__ == '__main__':
