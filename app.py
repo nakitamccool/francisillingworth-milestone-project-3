24b17131c46af4eeba64d9b89bef774edcf37903
Ubuntu <ubuntu@ip-172-31-91-138.ec2.internal>
2019-07-05 16:27:36 +0000

    added working function to edit and delete recipe

--- app.py
+++ app.py
@@ -39,6 +39,29 @@ def edit_recipe(recipe_id):
     all_cuisine = mongo.db.cuisine.find()
     return render_template('editrecipe.html', recipe = the_recipe, cuisine=all_cuisine)
     
+    
+    
+@app.route('/update_recipe/<recipe_id>', methods = ["POST"])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipe
+    recipes.update( {'_id': ObjectId(recipe_id)},
+    {
+        'recipe_name' : request.form.get('recipe_name'),
+        'cuisine_type' : request.form.get('cuisine_type'),
+        'author' : request.form.get('author'),
+        'ingredients' : request.form.get('ingredients'),
+        'method' : request.form.get('method'),
+        'cook_time' : request.form.get('cook_time'),
+    })
+    return redirect(url_for('get_recipes'))
+
+@app.route('/delete_task/<recipe_id>')
+def delete_recipe(recipe_id):
+    mongo.db.recipe.remove({'_id':ObjectId(recipe_id)})
+    return redirect(url_for('get_recipes'))
+    
+
+    
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
