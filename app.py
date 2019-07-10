1c492c0812c6e1dd8593db01158ff6953da0f556
Ubuntu <ubuntu@ip-172-31-91-138.ec2.internal>
2019-07-10 12:51:42 +0000

    moved env var to heroku

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 db_password = os.getenv('DB_PASSWORD')
 
 app.config["MONGO_DBNAME"] = 'recipe_project'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-aogax.mongodb.net/recipe_project?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 
 
 mongo = PyMongo(app) 
 
